2f3d0da31872635329d8a27c7c84694e501381a2
SANKET BHAT <sanketbhat882002@gmail.com>
2023-03-20 19:30:16 +0530

    added logo to Login page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -7,21 +7,24 @@ import { ReactElement, ReactNode } from "react";
 
 import customTheme from '../Themes/CustomSupaTheme'
 
-
-
+import Image from "next/image";
+ 
 const Home: NextPageWithLayout = () => {
   const session = useSession();
   const supabase = useSupabaseClient();
   
   return (
-    <div className="container place-self-center max-w-md" style={{ padding: "50px 0 100px 0" }}>
+    <div className="container max-w-md" style={{ padding: "50px 0 100px 0" }}>
       {!session ? (
+        <div>
+        <Image className='py-2 mx-auto shadow-lg' src="./images/logo-dark.svg" alt="Logo" width={200} height={200} />
         <Auth
           supabaseClient={supabase}
           appearance={{ theme: customTheme }}
           theme="evenDarker"
           providers={['google', 'facebook', 'twitter']}
         />
+        </div>
       ) : (
         <Account session={session} />
       )}
